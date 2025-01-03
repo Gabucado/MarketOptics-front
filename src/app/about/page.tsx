e9afe377dc2598d2aabc1f656018977c2cfc02c5
Gabucado <awsalinas@uc.cl>
2025-01-03 00:19:42 -0300

    📖 Docs

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+const AboutPage: FC = () => {
+  return <div>
+    <div className='text-left'>
+        <h1 className='text-4xl font-bold mb-4'>Modelo de Datos</h1>
+        <p className='text-md mt-2'>
+          El modelo de datos incluye las siguientes relaciones:
+        </p>
+        
+        <ul className='list-disc list-inside mt-2'>
+          <li><strong>Producto:</strong> Tiene un nombre.</li>
+          <li><strong>Venta:</strong> Tiene una fecha.</li>
+          <li><strong>Inventario:</strong> Tiene un Producto, una fecha y una cantidad.</li>
+          <li><strong>VentaProducto:</strong> Tiene una venta, un producto, un valor y una cantidad.</li>
+        </ul>
+      </div>
+
+      <div className='mt-6'>
+        <h2 className='text-2xl font-semibold'>Diagrama</h2>
+        <img src="/Diagram.svg" alt="SVG Diagram" className='mt-4' />
+      </div>
+  </div>
+}
+
+export default AboutPage;
